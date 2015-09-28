bbf5b1907f353f7fae6dcb68e403f302cd914c08
Ruta <ruta.urkiene@gmail.com>
2015-09-28 20:48:38 +0300

    Changes
    Nedideli pakeitimai loadjson.js.

--- loadjson.js
+++ loadjson.js
@@ -1,12 +1,10 @@
 $(document).ready(function(){
-	$("input.icon-cart").on("click", function(){	
 	$.getJSON("list.json")
 	.done(function(result) {
 		printList(result);
 	});
 	function printList(result) {
-		console.log(result.list.myitems);
+		console.log(result.myitems);
 	}
 	
-});
 });
